d1fd2f0a58fa1ee08b98f5a9f23ed02dd0e787f6
Laura Chapa <laurachg8@gmail.com>
2019-03-07 11:24:42 -0600

    mongo env

--- server.js
+++ server.js
@@ -17,13 +17,13 @@ mongoose
   .then(() => console.log("Database connected successfully"))
   .catch(err => console.log(err));
 
-const path = require("path");
-
-app.use(express.static(path.join(__dirname, "build")));
-
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.get("/posts", (req, res) => {
   console.log("getting posts");
